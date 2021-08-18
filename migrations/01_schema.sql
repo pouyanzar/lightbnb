aef64c7777c8b52716615f7b3483ae1aa1554586
pouyanzar <pouyan.zarbafian@hotmail.com>
2021-08-18 19:13:40 +0000

    fixed a bug

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -12,21 +12,24 @@ CREATE TABLE users (
 
 CREATE TABLE properties (
   id SERIAL PRIMARY KEY NOT NULL,
+  owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+
   title VARCHAR(255) NOT NULL,
-  description TEXT NOT NULL,
+  description TEXT,
   thumbnail_photo_url VARCHAR(255) NOT NULL,
   cover_photo_url VARCHAR(255) NOT NULL,
-  const_per_night INTEGER NOT NULL,
-  parking_spaces INTEGER NOT NULL,
-  number_of_bathrooms INTEGER NOT NULL,
-  number_of_bedrooms INTEGER NOT NULL,
+  cost_per_night INTEGER  NOT NULL DEFAULT 0,
+  parking_spaces INTEGER  NOT NULL DEFAULT 0,
+  number_of_bathrooms INTEGER  NOT NULL DEFAULT 0,
+  number_of_bedrooms INTEGER  NOT NULL DEFAULT 0,
+
   country VARCHAR(255) NOT NULL,
   street VARCHAR(255) NOT NULL,
   city VARCHAR(255) NOT NULL,
   province VARCHAR(255) NOT NULL,
   post_code VARCHAR(255) NOT NULL,
-  active BOOLEAN NOT NULL,
-  owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE 
+
+  active BOOLEAN NOT NULL DEFAULT TRUE
 );
 
 CREATE TABLE reservations (
@@ -39,9 +42,9 @@ CREATE TABLE reservations (
 
 CREATE TABLE property_reviews (
   id SERIAL PRIMARY KEY NOT NULL,
-  rating SMALLINT NOT NULL,
-  message TEXT NOT NULL,
   guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
-  reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE
+  reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
+  rating SMALLINT NOT NULL DEFAULT 0,
+  message TEXT
 );
